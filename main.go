5b0c366e004effbcc0aef961eaba1c3e6e4f90ac
jacobadev <junior1232131@gmail.com>
2024-11-17 14:19:02 -0300

    feat: add utils

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
 package main
-
-func MergeHeaders(commonHeaders map[string]string, queryIDheaders map[string]string) map[string]string {
-	// Create a copy of the commonHeaders to avoid modifying the original map
-	mergedHeaders := make(map[string]string)
-
-	// Copy commonHeaders into mergedHeaders
-	for key, value := range commonHeaders {
-		mergedHeaders[key] = value
-	}
-
-	// Add or overwrite with queryIDheaders
-	for key, value := range queryIDheaders {
-		mergedHeaders[key] = value
-	}
-
-	return mergedHeaders
-}
